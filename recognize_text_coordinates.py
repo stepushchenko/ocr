4a61b881ceeac79667e8eb7cf39dfeee40ef1d6d
Ilia Stepushchenko <ilia@Ilias-MacBook-Air.local>
2023-05-30 14:34:44 +0300

    easyocr installation

--- recognize_text_coordinates.py
+++ recognize_text_coordinates.py
@@ -1,39 +1,20 @@
-# for Mac: brew install tesseract
-# for Windows: pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+import cv2
+import easyocr
+# import matplotlib.pyplot as plt
 
-from pytesseract import pytesseract, Output
-from PIL import Image
+image_path = 'resources/screenshots/text.png'
 
+# prepare image
+img = cv2.imread(image_path)
 
-class Lang:
-    ENG = 'eng'
-    RUS = 'rus'
-    GERMAN = 'deu'
+# instance text detector
+reader = easyocr.Reader(['en'], gpu=False)
 
+# detect text on image
+text_ = reader.detect(img)
 
-class GetCoordinates:
+for t in text_:
+    print(t)
 
-    def get_center_coordinate(self, img: str, word: str, lang: str):
-
-        img = Image.open(img)  # open image
-        image_data = pytesseract.image_to_data(img, lang=lang, output_type=Output.DICT)  # prepare image for analyzing
-
-        if word in image_data['text']:
-            index = image_data['text'].index(word)  # get the current word position in the list
-            left = image_data['left'][index]  # left point coordinate
-            top = image_data['top'][index]  # top point coordinate
-            width = image_data['width'][index]  # the width
-            height = image_data['height'][index]  # the height
-            result_left = left + round(width/2)
-            result_top = top + round(height/2)
-            return f'Coordinates of the center of the word {word} are: {result_left}-{result_top}'
-
-
-if __name__ == '__main__':
-    get_coordinates = GetCoordinates()
-
-    print(get_coordinates.get_center_coordinate(
-        img='resources/screenshots/mb.png',
-        lang=Lang.GERMAN,
-        word='Abstands-Assistent'
-    ))
+# issue with pyend
+# more details here: https://github.com/pandas-dev/pandas/issues/27532#issuecomment-514044754
