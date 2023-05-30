b855fabb9f8339d0f0fae85904bf28b28003caa8
Ilia Stepushchenko <ilia@Ilias-MacBook-Air.local>
2023-05-30 12:15:05 +0300

    add notes

--- recognize_unique_img_coordinates.py
+++ recognize_unique_img_coordinates.py
@@ -1,3 +1,33 @@
+"""
+Let's now go over this code.
+We import OpenCV module.
+We then create a variable, image, which contains the image we want to search. In this case, it is 'Rainforest.png'.
+We show this image.
+We then create a grayscale version of this image. This simplifies the image.
+
+We then create another variable, template, which represents the subset image that we want to search for within the
+larger image, image ('Rainforest.png'). This image is named 'Yellowing-leaf.png'.
+
+We then have another variable, result, which stores basically whether there is a match found.
+
+We then create a tuple of values that allows us to get the location of the match, assuming there is a match. The
+variable, max_loc, represents the
+
+Next, we have another tuple, height and width, that contain the height and width of the template image, or the target
+image we are searching for within the larger image. We have these values because we are going to highlight the image
+once found.
+
+We then have a variable, opposite_corner, which represents the bottom right of the rectangle we will create. If we
+take max_loc[0] and add the width and max_loc[1] and add the height, we have obtained the bottom right corner.
+
+max_loc represents the top left corner.
+We then use the cv2.rectangle() function to draw a rectangle around the match. We do this in a red color with a line
+thickness of 5.
+
+We then show the image.
+And this is how we can match an image embedded in another image in Python using OpenCV.
+"""
+
 import cv2
 
 # paths to the images
