fb75d0cf306ea59140ff195fba1a149472146644
Ilia Stepushchenko <ilia@Ilias-MacBook-Air.local>
2023-05-30 15:49:25 +0300

    refactoring

--- get_multiply_img_coordinates_from_screenshot.py
+++ get_multiply_img_coordinates_from_screenshot.py
@@ -19,11 +19,10 @@ w, h = template.shape[::-1]
 res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 threshold = 0.8
 loc = np.where(res >= threshold)
-center_coordinates = []
+
+# draw a rectangles
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-# todo: prepare a dict with coordinates of the center of each found image
 
 # show big image with red rectangle
 cv2.imshow('ChessOne', img_rgb)
