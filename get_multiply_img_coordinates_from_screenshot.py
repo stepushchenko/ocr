607a7e12029e31afacc84a5cb66c9f521def7bbf
Gev Iritsyan <gevorg.iritsyan2@dxc.com>
2023-06-08 15:06:07 +0300

    refactor and find center of each shape

--- get_multiply_img_coordinates_from_screenshot.py
+++ get_multiply_img_coordinates_from_screenshot.py
@@ -22,16 +22,18 @@ loc = np.where(res >= threshold)
 
 # get the center of each shape
 centers = set()
+i = 0
+
 for pt in zip(*loc[::-1]):
-    center_x = pt[0] + w // 2
-    center_y = pt[1] + h // 2
-    center = (center_x, center_y)
-    centers.add(center)
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-    cv2.circle(img_rgb, center, 3, (0, 255, 2), 1)
-    break
-
-
+    if i == 0 or i % 9 == 0:
+        center_x = pt[0] + w // 2
+        center_y = pt[1] + h // 2
+        center = (center_x, center_y)
+        centers.add(center)
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        cv2.circle(img_rgb, center, 3, (0, 255, 2), 1)
+        print(center)
+    i += 1
 
 # show big image with red rectangles and green centers
 cv2.imshow('ChessOne', img_rgb)
@@ -40,6 +42,6 @@ cv2.waitKey(0)
 # sort the centers based on y-coordinate and x-coordinate
 sorted_centers = sorted(centers, key=lambda c: (c[1], c[0]))
 
-# print the sorted centers of each shape
-for center in sorted_centers:
-    print(f"Shape center: {center}")
+# # print the sorted centers of each shape
+# for center in sorted_centers:
+#     print(f"Shape center: {center}")
